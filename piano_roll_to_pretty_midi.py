c26da7756550c9bf459883db704900e1bcc4b08f
Mu-jun <suo24448@gmail.com>
2022-07-08 15:58:02 +0900

    fix : velocity clipping

--- piano_roll_to_pretty_midi.py
+++ piano_roll_to_pretty_midi.py
@@ -1,30 +1,14 @@
 import numpy as np
 import pretty_midi_fluidsynth as pmf
 
-def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
-    '''Convert a Piano Roll array into a PrettyMidi object
-     with a single instrument.
-    Parameters
-    ----------
-    piano_roll : np.ndarray, shape=(128,frames), dtype=int
-        Piano roll of one instrument
-    fs : int
-        Sampling frequency of the columns, i.e. each column is spaced apart
-        by ``1./fs`` seconds.
-    program : int
-        The program number of the instrument.
-    Returns
-    -------
-    midi_object : pretty_midi.PrettyMIDI
-        A pretty_midi.PrettyMIDI class instance describing
-        the piano roll.
-    '''
-    notes, frames = piano_roll.shape
+def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0, start_pitch=24):
+    
+    notes = piano_roll.shape[0] + start_pitch
     pm = pmf.PrettyMIDI()
     instrument = pmf.Instrument(program=program)
 
     # pad 1 column of zeros so we can acknowledge inital and ending events
-    piano_roll = np.pad(piano_roll, [(0, 0), (1, 1)], 'constant')
+    piano_roll = np.pad(piano_roll, [(start_pitch, 0), (1, 1)], 'constant')
 
     # use changes in velocities to find note on / note off events
     velocity_changes = np.nonzero(np.diff(piano_roll).T)
@@ -36,11 +20,14 @@ def piano_roll_to_pretty_midi(piano_roll, fs=100, program=0):
     for time, note in zip(*velocity_changes):
         # use time + 1 because of padding above
         velocity = piano_roll[note, time + 1]
+        if velocity > 126:
+            velocity = 126
         time = time / fs
         if velocity > 0:
             if prev_velocities[note] == 0:
                 note_on_time[note] = time
                 prev_velocities[note] = velocity
+
         else:
             pm_note = pmf.Note(
                 velocity=prev_velocities[note],
